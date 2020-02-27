d37142b8f7684595c35b2854efeb8edb50cb1797
Dani Louca <dlouca@splunk.com>
2020-02-27 00:10:21 -0500

    Adding SSL and user auth support
    Signed-off-by: Dani Louca <dlouca@splunk.com>

--- mongodb/README.md
+++ mongodb/README.md
@@ -1,19 +1,4 @@
 
-
-## Build
-Run the below to build the image
-```
-docker build -t <repo/name:tag> .
-```
-## Run
-`--net host` is only needed if you mongodb is installed on the same docker host and you are trying
-to access it through localhost
-
-```
-docker run -it --net host <repo/name:tag>
-```
-
-
 # MongoDB Connection Test
 This docker image is used to test customer's connection to MongoDB; using the specific version of pymongo the smart agent uses.
 
--- mongodb/connectionTest.py
+++ mongodb/connectionTest.py
@@ -7,12 +7,26 @@ except NameError:
     pass
 host = input("Enter mongoDB host: ")
 port = input("Enter mongoDB port: ")
+user = input("Enter mongoDB username: ")
+pasw = input("Enter mongoDB password: ")
+
+def str_to_bool(s):
+    if s == 'True':
+         return True
+    elif s == 'False':
+         return False
+    else:
+         raise ValueError
 
 def get_extra_options():
     d = {}
     if "ssl_ca_certs" in os.environ:
       d["ssl_ca_certs"]=os.environ["ssl_ca_certs"]
-    #Add more when needed  
+    if "ssl" in os.environ:
+      d["ssl"]=str_to_bool(os.environ["ssl"])
+    if "tlsInsecure" in os.environ:
+      d["tlsInsecure"]=str_to_bool(os.environ["tlsInsecure"])
+    #Add more when needed
     return d
 
 
@@ -22,7 +36,7 @@ if len(options) > 0:
 
 try:
     options = get_extra_options()
-    client = pymongo.MongoClient(host, int(port), **options)
+    client = pymongo.MongoClient(host, int(port), username = user , password = pasw, **options)
 except Exception as e:
     print("Failed to connect to host %s port %s" % (host, port))
     print(e)
